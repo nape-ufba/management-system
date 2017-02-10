72dcdb15aee30eff157e9db63335abf7ea06db04
Rodrigo Araujo <dygufa@gmail.com>
2017-02-10 20:17:39 -0300

    incrementing sing up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :course_id, :type_id, :role_id, :special_need_id, :phone, :document_number)
+      params.require(:user).permit(:name, :email, :course_id, :user_type_id, :role_id, :special_need_id, :phone, :document_number)
     end
 end
--- app/views/users/_user.json.jbuilder
+++ app/views/users/_user.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! user, :id, :name, :email, :course_id, :type_id, :role_id, :special_need_id, :phone, :document_number, :created_at, :updated_at
+json.extract! user, :id, :name, :email, :course_id, :user_type_id, :role_id, :special_need_id, :phone, :document_number, :created_at, :updated_at
 json.url user_url(user, format: :json)
